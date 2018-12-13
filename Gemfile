f549e9c6632c1c5496d73fe442ef20aa90bd9821
ravi4rails <ravionrails@gmail.com>
2018-12-13 19:03:54 +0530

    setup devise and cancancan

--- Gemfile
+++ Gemfile
@@ -68,3 +68,10 @@ end
 group :production do 
   gem 'pg'
 end
+
+gem 'bootstrap', '~> 4.1.3'
+gem 'jquery-rails'
+gem "font-awesome-rails"
+gem 'devise', '~> 4.4.3'
+gem 'cancancan', '~> 2.0'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'users', controllers: {
+    sessions:             'users/sessions',
+    passwords:            'users/passwords',
+    registrations:        'users/registrations',
+    confirmations:        'users/confirmations',
+    ominiauth_callbacks:  'users/ominiauth_callbacks',
+    unlocks:              'users/unlocks'
+  }
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
